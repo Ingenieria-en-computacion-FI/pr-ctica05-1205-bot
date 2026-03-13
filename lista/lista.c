b0556393c3c184e37a4207f9497ac7c699130ee9
unknown <ypollo20@gmail.com>
2026-03-12 18:57:09 -0600

    Subo lista.c

--- lista/lista.c
+++ lista/lista.c
@@ -8,20 +8,20 @@ Lista* lista_crear() {
     Lista *lista = NULL;
     lista = (Lista*)malloc(sizeof(Lista));
 
-    lista->head = NULL;
-    lista->tail = NULL;
+    lista->head = lista->tail = NULL;
 
     return lista;
 }
 
-int lista_vacia(Lista* lista) {
-    return lista->head == NULL;
+bool lista_vacia(Lista *lista) {
+    if(lista->head == NULL) return true;
+    return false;
 }
 
 void lista_insertar_head(Lista* lista, int dato) {
     Nodo* nuevo = (Nodo*)malloc(sizeof(Nodo));
     nuevo->dato = dato;
-    nuevo->siguinte = NULL;
+    nuevo->siguiente = NULL;
 
     if(lista_vacia(lista)) {
         lista->head = nuevo;
@@ -47,15 +47,15 @@ void lista_insertar_tail(Lista* lista, int dato) {
 }
 
 int lista_eliminar_head(Lista* lista) {
-    if(lista_vacia(lista)) return -1;
-
-    Nodo *temp = lista->head;
-    int dato = temp->dato;
-
-    lista->head = temp->siguiente;
-
-    if(lista->head == NULL) lista->tail = NULL;
-    free(temp);
+    if(lista_vacia(lista)) {
+        printf("Errror");
+        return -1;
+    }
+    Nodo *aux_head = lista->head;
+    int dato = aux_head->dato;//aqui no nos pasan dato pero lo necesitamos para devolverlo al final de la función
+    lista->head = aux_head->siguiente;
+    if(lista->head == NULL) lista->tail = NULL;// si solo habia un nodo, tail queda en NULL
+    free(aux_head);
     return dato;
 }
 
@@ -65,32 +65,31 @@ int lista_eliminar_tail(Lista* lista) {
         return -1;
     }
 
-    Nodo *actual = lista->head;
-    int dato;
+    Nodo *aux_head = lista->head;
+    int dato; 
 
     if(lista->head == lista->tail) {
-        dato = actual->head;
-        free(actual);
+        dato = aux_head->dato;
+        free(aux_head);
         lista->head = NULL;
         lista->tail = NULL;
         return dato;
     }
 
-    while(actual->siguiente != lista->tail) {
-        actual = actual->siguiente;
+    while(aux_head->siguiente != lista->tail) {
+        aux_head = aux_head->siguiente;
     }
 
     dato = lista->tail->dato;
 
     free(lista->tail);
-    lista->tail = actual;
+    lista->tail = aux_head;
     lista->tail->siguiente = NULL;
 
     return dato;
 }
 
-void lista_imprimir(Lista* lista)
-{
+void lista_imprimir(Lista* lista) {
     Nodo* actual = lista->head;
 
     while(actual != NULL)
@@ -103,8 +102,8 @@ void lista_imprimir(Lista* lista)
 }
 
 void lista_destruir(Lista* lista) {
-    while( !lista_vacia(lista)) {
-        lista_eliminar_head(lista);
+    while( !lista_vacia(lista)) {//mientras la lista no este vacia
+        lista_eliminar_head(lista);//podria usar lista_eliminar_tail pero es menos eficiente
     }
 
     free(lista);
